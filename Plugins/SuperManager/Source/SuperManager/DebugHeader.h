dda9d2c22d93b4f700b0d8eb938ca34e55a05f35
PengChaoJay <zpcwyy@gmail.com>
2023-07-18 00:25:08 +0800

    添加调试

--- Plugins/SuperManager/Source/SuperManager/DebugHeader.h
+++ Plugins/SuperManager/Source/SuperManager/DebugHeader.h
@@ -1,7 +1,41 @@
 #pragma once
+#include "Framework/Notifications/NotificationManager.h"
+#include "Misc/MessageDialog.h"
+#include "Widgets/Notifications/SNotificationList.h"
 
-class DebugHeader
+
+namespace  DEbugHeader
 {
-public:
-	
-};
+	static void Pirnt(const FString &Message,const FColor &Color = FColor::Cyan)
+	{
+		if(GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1,8.f,Color,Message);
+		}
+	}
+	static void PrintLog(const FString & Message)
+	{
+		UE_LOG(LogTemp,Warning,TEXT("%s"),*Message);
+	}
+
+	static EAppReturnType::Type ShowMsgDialog(EAppMsgType::Type MsgType,const FString & Message,bool bShowMsgAsWarning = true)
+	{
+		if(bShowMsgAsWarning)
+		{
+			FText MsgTitle = FText::FromString("Wearning");
+			return FMessageDialog::Open(MsgType,FText::FromString(Message),&MsgTitle);
+		}
+		else
+		{
+			return FMessageDialog::Open(MsgType,FText::FromString(Message));
+		}
+	}
+	static void ShowNotifyInfo(const FString &Message)
+	{
+		FNotificationInfo NotifyInfo(FText::FromString(Message));
+		NotifyInfo.bUseLargeFont = true;
+		NotifyInfo.FadeOutDuration = 7.0f;
+
+		FSlateNotificationManager::Get().AddNotification(NotifyInfo);
+	}
+}
